bbfe03beb7d7b7cae063ff1e234668cab47491fd
Lee <leeblazek@gmail.com>
2012-04-28 13:07:54 +1200

    commit of 0.3 mobile module

---
+++ _config.php
@@ -0,0 +1,6 @@
+<?php
+//comment
+define('MOBILE_DIR', 'mobile');
+
+Object::add_extension('SiteConfig', 'MobileSiteConfigExtension');
+Object::add_extension('ContentController', 'MobileSiteControllerExtension');
